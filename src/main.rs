6344035a95f4eac1af80f2d9c4c1bb068b14eeb8
tx0sh1 <clydemojura@protonmail.com>
2025-09-03 23:24:31 +0200

    build: implement add and print commands
    - Users can now add items to the todo list via the add command
    - Added placeholder print command using debug formatting to show vector contents
    - Refactored variable names in add logic for clarity

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::io;
 fn main() {
     println!("Hello Welcome to this TODO program");
 
+    let mut todo_items :Vec<String> = Vec::new();
 
     loop {
         println!("Input function: ADD / Print / Delete / quit");
@@ -12,8 +13,15 @@ fn main() {
         let command = name.trim().to_lowercase();
 
         match command.as_str() { // .as_str() lets us match on string slices
-            "add" | "a" => println!("Add works"),
-            "print" | "p" => println!("print works"),
+            "add" | "a" => {
+                println!("Enter the items you want to add");
+
+                let mut user_input = String::new();
+                io::stdin().read_line(&mut user_input).expect("failed to read line");
+                let trimmed_item = user_input.trim().to_string();
+                todo_items.push(trimmed_item);
+            },
+            "print" | "p" => println!("here are your items: {:?}", todo_items),
             "delete" | "d" => println!("delete works"),
             "quit" | "q" => {
                 println!("Thank you!");
